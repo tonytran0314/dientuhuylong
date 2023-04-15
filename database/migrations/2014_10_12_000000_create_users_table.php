a574d5f3c96f49b926ed3249a9ee68a80f93b8bb
tonytran0314 <tonytran0314@gmail.com>
2023-04-15 15:41:50 +0700

    edit string length

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email', 254)->unique();
+            $table->string('email', 128)->unique();
             $table->string('image')->nullable();
             $table->string('address');
             $table->string('phone_number');

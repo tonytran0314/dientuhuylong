f42661410a41c638f178f22e2d15e794df2457d8
tonytran0314 <tonytran0314@gmail.com>
2023-04-16 00:48:20 +0700

    nav bar share view

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -8,13 +8,13 @@
 					<!-- NAV -->
 					<ul class="main-nav nav navbar-nav">
 						<li class="active"><a href="#">Home</a></li>
-						{{-- <li id="category_parent"><a>Tất cả thế loại</a>
+						<li id="category_parent"><a>Tất cả thế loại</a>
 							<ul id="category_child">
 								@foreach($categories_list as $category)
 									<li><a href="{{ route('product.byCate', $category->slug) }}">{{ $category->name }}</a></li>
 								@endforeach
 							</ul>
-						</li> --}}
+						</li>
 						<li><a href="#">Sản phẩm mới nhất</a></li>
 						<li><a href="#">Giảm giá sốc</a></li>
 						<li><a href="#">Liên hệ chúng tôi</a></li>

9d67109685d30a9a841b4a408d58229440e57455
tonytran0314 <tonytran0314@gmail.com>
2023-04-16 14:35:54 +0700

    comment navigation bar

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -8,13 +8,13 @@
 					<!-- NAV -->
 					<ul class="main-nav nav navbar-nav">
 						<li class="active"><a href="{{ route('home') }}">Home</a></li>
-						<li id="category_parent"><a>Tất cả thế loại</a>
+						{{-- <li id="category_parent"><a>Tất cả thế loại</a>
 							<ul id="category_child">
 								@foreach($categories_list as $category)
 									<li><a href="{{ route('product.byCate', $category->slug) }}">{{ $category->name }}</a></li>
 								@endforeach
 							</ul>
-						</li>
+						</li> --}}
 					</ul>
 					<!-- /NAV -->
 				</div>

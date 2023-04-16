89c095b9922caf90e36a23f3f7777d65f910be34
tonytran0314 <tonytran0314@gmail.com>
2023-04-16 19:28:00 +0700

    edit navigation bar

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -8,13 +8,18 @@
 					<!-- NAV -->
 					<ul class="main-nav nav navbar-nav">
 						<li class="active"><a href="{{ route('home') }}">Home</a></li>
-						{{-- <li id="category_parent"><a>Tất cả thế loại</a>
+						<li id="category_parent"><a>Tất cả thế loại</a>
 							<ul id="category_child">
 								@foreach($categories_list as $category)
 									<li><a href="{{ route('product.byCate', $category->slug) }}">{{ $category->name }}</a></li>
 								@endforeach
 							</ul>
-						</li> --}}
+						</li>
+						<li><a href="{{ route('product.byCate', 'noi-chien-khong-dau') }}">Nồi chiên không dầu</a></li>
+						<li><a href="{{ route('product.byCate', 'tivi') }}">Tivi</a></li>
+						<li><a href="{{ route('product.byCate', 'loa') }}">Loa</a></li>
+						<li><a href="{{ route('product.byCate', 'ban-phim') }}">Bàn phím</a></li>
+						<li><a href="{{ route('product.byCate', 'chuot-may-tinh') }}">Chuột máy tính</a></li>
 					</ul>
 					<!-- /NAV -->
 				</div>
